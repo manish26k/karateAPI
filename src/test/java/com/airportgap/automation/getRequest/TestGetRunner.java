1612f9a86850c14e047a2ec27d56604f98ee4bf7
manish26k <mani26kg@gmail.com>
2025-07-23 20:23:16 +0530

    update the testgetrunner and yml file

--- src/test/java/com/airportgap/automation/getRequest/TestGetRunner.java
+++ src/test/java/com/airportgap/automation/getRequest/TestGetRunner.java
@@ -8,6 +8,9 @@ import com.intuit.karate.junit5.Karate.Test;
 public class TestGetRunner {
     @Test
     public Karate runTest(){
-        return Karate.run("getAirports").relativeTo(getClass());
+        return Karate.run("getAirports")
+                     .relativeTo(getClass())
+                     .outputCucumberJson(true)
+                     .reportDir("target/karate-reports");
     }
 }
